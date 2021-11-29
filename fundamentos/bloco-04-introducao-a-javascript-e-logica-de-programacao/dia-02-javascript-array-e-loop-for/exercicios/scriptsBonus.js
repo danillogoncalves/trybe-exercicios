a5000cc96faff4b2966585c34f1b485173b5416d
danillo <danillogonvalves001@gmail.com>
2021-11-29 18:49:18 -0300

    Finalizado os exercícios bônus.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios/scriptsBonus.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios/scriptsBonus.js
@@ -1,1 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let novo = [];
+
+for (let i = 0; i < numbers.length; i += 1) {
+    if (i == numbers.length -1) {
+        novo.push(numbers[i]*2);
+    } 
+    else {
+    novo.push(numbers[i]*numbers[i+1]);
+    }
+}
+
+console.log(novo);
---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios/scriptsBonusDecrescente.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let i1 = 1; i1 < numbers.length; i1 += 1) {
+    for (let i2 = 0; i2 < numbers.length; i2 += 1) {
+        if (numbers[i1] > numbers[i2]) {
+            let valorI1 = numbers[i1];
+            numbers[i1] = numbers[i2];
+            numbers[i2] = valorI1;
+        }
+    }
+}
+
+console.log(numbers);
