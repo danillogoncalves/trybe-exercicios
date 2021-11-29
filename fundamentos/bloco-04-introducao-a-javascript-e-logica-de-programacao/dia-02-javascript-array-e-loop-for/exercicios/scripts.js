4c9c4187ddb61ac421204fbfe25547fce8774f0c
danillo <danillogonvalves001@gmail.com>
2021-11-29 15:46:58 -0300

    Finalizado os exercícios.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios/scripts.js
@@ -2,8 +2,10 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
 let mean = 0;
 let bigger = 0;
+let smaller = 0;
+let even = false;
 
-console.log('1.')
+console.log('1.');
 
 for (let i of numbers) {
     console.log(i);
@@ -11,7 +13,7 @@ for (let i of numbers) {
 
 console.log('-*-*-*-*-*-*-*-*-*-*-');
 
-console.log('2.')
+console.log('2.');
 
 for (let number of numbers) {
     sum += number;
@@ -20,13 +22,13 @@ console.log(sum);
 
 console.log('-*-*-*-*-*-*-*-*-*-*-');
 
-console.log('3.')
+console.log('3.');
 
 console.log(mean = sum / numbers.length);
 
 console.log('-*-*-*-*-*-*-*-*-*-*-');
 
-console.log('4.')
+console.log('4.');
 
 if (mean > 20) {
     console.log("valor maior que 20");
@@ -37,7 +39,7 @@ else {
 
 console.log('-*-*-*-*-*-*-*-*-*-*-');
 
-console.log('5.')
+console.log('5.');
 
 for (let i =  0; i < numbers.length;i += 1) {
     if (numbers[i] > bigger) {
@@ -48,4 +50,49 @@ console.log(bigger);
 
 console.log('-*-*-*-*-*-*-*-*-*-*-');
 
-console.log('6.')
+console.log('6.');
+
+for (let i = 0; i < numbers.length;i += 1) {
+    if (numbers[i] % 2 == 1) {
+        even = true;
+        break;
+    }
+}
+
+for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] % 2 == 1) {
+        console.log(numbers[i]);
+    }
+}
+if (even == false) {
+    console.log("nenhum valor ímpar encontrado");
+}
+console.log('-*-*-*-*-*-*-*-*-*-*-');
+
+console.log('7.');
+smaller = numbers[0];
+for (let i = 1; i < numbers.length; i += 1) {
+    if (smaller > numbers[i]) {
+        smaller = numbers[i];
+    }
+}
+console.log(smaller);
+
+console.log('-*-*-*-*-*-*-*-*-*-*-');
+
+console.log('8.');
+let score = [];
+for (let i = 1; i <= 25; i += 1) {
+    score.push(i);
+}
+console.log(score);
+
+console.log('-*-*-*-*-*-*-*-*-*-*-');
+
+console.log('9.');
+
+for (let number of numbers) {
+    console.log(number / 2);
+}
+
+console.log('-*-*-*-*-*-*-*-*-*-*-');
