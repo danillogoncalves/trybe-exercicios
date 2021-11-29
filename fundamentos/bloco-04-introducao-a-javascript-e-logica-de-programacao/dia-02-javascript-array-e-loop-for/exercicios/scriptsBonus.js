b1d09471295019d4dd026888fc17f8fc88bda557
danillo <danillogonvalves001@gmail.com>
2021-11-29 19:26:50 -0300

    Finalizado os exercícios bônus e dando os últimos toques.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios/scriptsBonus.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios/scriptsBonus.js
@@ -2,7 +2,9 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let novo = [];
 
 for (let i = 0; i < numbers.length; i += 1) {
-    if (i == numbers.length -1) {
+    // if (typeof numbers[i+1] === 'undefined') {
+    // if (i == numbers.length -1) {
+    if (!numbers[i+1]) {
         novo.push(numbers[i]*2);
     } 
     else {
--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios/scriptsBonusCrescente.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios/scriptsBonusCrescente.js
@@ -1,7 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let i1 = 1; i1 < numbers.length; i1 += 1) {
+    /* console.log(numbers[i1]); */
     for (let i2 = 0; i2 < numbers.length; i2 += 1) {
+        /* console.log(numbers[i2]); */
         if (numbers[i1] < numbers[i2]) {
             let valorI1 = numbers[i1];
             numbers[i1] = numbers[i2];
@@ -10,4 +12,4 @@ for (let i1 = 1; i1 < numbers.length; i1 += 1) {
     }
 }
 
-console.log(numbers);
+/* console.log(numbers); */
