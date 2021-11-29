7862b7ef13924fa65d7bc177b3edac622f796b54
danillo <danillogonvalves001@gmail.com>
2021-11-29 09:38:10 -0300

    Realizado o segundo exercício.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/conteudos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/conteudos/scripts.js
@@ -1,4 +1,18 @@
 let menu = ["Home", "Serviços", "Portfólio", "Links"];
-let menuServices = menu.indexOf("Serviços");
+let menuServices = menu[1];
 
 console.log(menuServices);
+
+let indexOfPortfolio = menu.indexOf("Portfólio");
+
+console.log(indexOfPortfolio);
+
+menu.push("Contato");
+
+console.log(menu);
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Merlancia'];
+
+for (let i = 0 ; i < groceryList.length; i += 1) {
+    console.log(groceryList[i]);
+}
