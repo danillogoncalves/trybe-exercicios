4d97e71316eadb1e57868e9c1787fad4ce15a1f3
danillo <danillogonvalves001@gmail.com>
2021-12-01 16:19:07 -0300

    Organizando as pastas dos CONTÉUDOS e dos EXERCÍCIOS.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios/scripts.js
@@ -3,14 +3,14 @@ let info = {
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
     recorrente: 'Sim',
-}
+};
 
 let info2 = {
     personagem: 'Tio Patinhas',
     origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
     nota: 'O último MacPatinhas',
     recorrente: 'Sim',
-}
+};
 
 console.log('Bem-vinda, '+ info.personagem)
 console.log(info);
