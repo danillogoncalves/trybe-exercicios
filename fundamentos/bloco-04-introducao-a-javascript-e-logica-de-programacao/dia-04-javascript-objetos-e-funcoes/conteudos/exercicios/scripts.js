5260b1268de4d79ebe40cebaa3971992cd06a355
danillo <danillogonvalves001@gmail.com>
2021-12-01 16:07:29 -0300

    Fiz até o sexto exercício da parte 1.

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/conteudos/exercicios/scripts.js
@@ -0,0 +1,42 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim',
+}
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+}
+
+console.log('Bem-vinda, '+ info.personagem)
+console.log(info);
+for (let key in info) {
+    console.log(key);
+}
+for (let key in info) {
+    if (info[key] === info2[key]) {
+        console.log('Ambos recorrentes');
+    }
+    else {
+    console.log(info[key] + ' e ' + info2[key]);
+}
+}
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+};
+let livrosFavoritos = leitor.livrosFavoritos[0];
+console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama '" + livrosFavoritos['titulo'] + "'")
