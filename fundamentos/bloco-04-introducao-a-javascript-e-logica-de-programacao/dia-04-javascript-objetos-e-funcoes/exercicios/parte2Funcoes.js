b6f188030a8330c1fe6eda1bc2d0375422d87683
danillo <danillogonvalves001@gmail.com>
2021-12-02 01:27:40 -0300

    Finalizado os exercícios de funções.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios/parte2Funcoes.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios/parte2Funcoes.js
@@ -37,6 +37,70 @@ function smallerIndexValue(array) {
     return smallerIndex;
 }
 
+function biggerNameCharacters(array) {
+    console.log('____________________________');
+    console.log('4.>');
+    console.log('');
+    let nameSize = array[0].length;
+    let biggerName = array[0];
+    for (let i = 1; i < array.length; i += 1) {
+        if (nameSize < array[i].length) {
+            nameSize = array[i].length;
+            biggerName = array[i];
+        }
+    }
+    return biggerName;
+}
+
+function integerRepeats(array) {
+    console.log('____________________________');
+    console.log('5.>');
+    console.log('');
+    let count = 0;
+    let countNumber = null;
+    let biggerRepeat = null;
+    for (let i = 0; i < array.length; i += 1) {
+        count = 0;
+        for (let s = 0; s < array.length; s += 1) {
+            if (array[i] === array[s]) {
+                count += 1;
+            }
+        }
+        if (count > countNumber) {
+            biggerRepeat = array[i];
+            countNumber = count;
+        }
+    }
+    return biggerRepeat;
+}
+
+function fromOneToNumber(num) {
+    console.log('____________________________');
+    console.log('6.>');
+    console.log('');
+    let sum = 0;
+    for (let i = 0; i < num; i += 1) {
+        sum += num - i;
+    }
+    return sum
+}
+
+function sameEnd(word, ending) {
+    console.log('____________________________');
+    console.log('7.>');
+    console.log('');
+    let end = false;
+    let cut = word.slice(- ending.length);
+    if (ending === cut) {
+        end = true
+    }
+    return end;
+}
+
 console.log(palindrome('aNa'));
 console.log(biggerIndexValue([2, 3, 6, 7, 10, 1]));
 console.log(smallerIndexValue([2, 4, 6, 7, 10, 0, -3]));
+console.log(biggerNameCharacters(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
+console.log(integerRepeats([2, 3, 2, 5, 8, 2, 3]));
+console.log(fromOneToNumber(5));
+console.log(sameEnd('trybe', 'be'));
