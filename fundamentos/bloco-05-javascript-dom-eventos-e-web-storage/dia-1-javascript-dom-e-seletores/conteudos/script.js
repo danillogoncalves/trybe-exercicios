70ec3f8ef72dbe64c2bfd163032357e5ead4def2
danillo <danillogonvalves001@gmail.com>
2021-12-03 18:18:33 -0300

    Começando os exercícios.

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/conteudos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/conteudos/script.js
@@ -1,12 +1,8 @@
 document.querySelector('header').style.backgroundColor = "#2fc18c";
 document.querySelector('.emergency-tasks').style.backgroundColor = "lightpink";
 document.querySelector('.no-emergency-tasks').style.backgroundColor = "yellowgreen";
-let emergency = document.querySelectorAll(".emergency-tasks div h3");
-for (let i = 0; i < emergency.length; i += 1) {
-    emergency[i].style.backgroundColor = "purple";
-}
-let noEmergency = document.querySelectorAll(".no-emergency-tasks div h3");
-for (let i = 0; i < emergency.length; i += 1) {
-    noEmergency[i].style.backgroundColor = "black";
-}
+
+document.querySelectorAll(".emergency-tasks div h3")[0].style.backgroundColor = 'purple';
+document.querySelectorAll("no-emergency-tasks div h3")[0].style.backgroundColor = 'black';
+
 document.querySelector('footer').style.backgroundColor = "navy";
