3a74978925b3138e7e8c5594059faab900bd8dba
danillo <danillogonvalves001@gmail.com>
2021-12-03 20:27:31 -0300

    Finalizado os fixação do conteúdo, fazendo os exercícios.

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/conteudos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/conteudos/script.js
@@ -1,8 +1,14 @@
 document.querySelector('header').style.backgroundColor = "#2fc18c";
 document.querySelector('.emergency-tasks').style.backgroundColor = "lightpink";
 document.querySelector('.no-emergency-tasks').style.backgroundColor = "yellowgreen";
+let emer = document.querySelectorAll(".emergency-tasks div h3");
+for (let i = 0; i < emer.length; i += 1) {
+    emer[i].style.backgroundColor = 'purple';
+}
 
-document.querySelectorAll(".emergency-tasks div h3")[0].style.backgroundColor = 'purple';
-document.querySelectorAll("no-emergency-tasks div h3")[0].style.backgroundColor = 'black';
+let noEmer = document.querySelectorAll(".no-emergency-tasks div h3");
+for (let i = 0; i < noEmer.length; i += 1) {
+    noEmer[i].style.backgroundColor = 'black';
+}
 
 document.querySelector('footer').style.backgroundColor = "navy";
