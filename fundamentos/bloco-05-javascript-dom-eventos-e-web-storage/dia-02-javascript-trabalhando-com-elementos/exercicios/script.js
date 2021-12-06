6c82af3348b1fdfd97ded071b59db5f5087e5f6d
danillo <danillogonvalves001@gmail.com>
2021-12-06 18:33:11 -0300

    Segunda parte do exercício foi feita.

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/exercicios/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/exercicios/script.js
@@ -2,3 +2,7 @@ let body = document.querySelector('body');
 let h1 = document.createElement('h1');
 h1.innerHTML = 'Exercício 5.2 - JavaScript DOM';
 body.appendChild(h1);
+
+let main = document.createElement('main');
+document.body.appendChild(main);
+main.className = 'main-content';
