d4cb5274bb34b449de8311e86c564da12b815bfe
danillo <danillogonvalves001@gmail.com>
2021-12-06 18:44:11 -0300

    Quarta parte do exercício foi feita.

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/exercicios/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/exercicios/script.js
@@ -4,8 +4,13 @@ h1.innerHTML = 'Exercício 5.2 - JavaScript DOM';
 body.appendChild(h1);
 
 let main = document.createElement('main');
-document.body.appendChild(main);
 main.className = 'main-content';
+document.body.appendChild(main);
 
 let sectCentet = document.createElement('section');
+sectCentet.className = 'center-content'
 document.querySelector('main').appendChild(sectCentet);
+
+let p = document.createElement('p');
+p.innerText = 'Algum texto';
+document.querySelector('.center-content').appendChild(p);
