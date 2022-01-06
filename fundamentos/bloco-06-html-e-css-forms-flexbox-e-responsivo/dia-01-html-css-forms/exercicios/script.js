29f267c1f2b82f1fac2a181dad5b190044f820e1
danillo <danillogonvalves001@gmail.com>
2022-01-06 00:46:32 -0300

    Falta fazer a validação da data na div.

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/exercicios/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/exercicios/script.js
@@ -7,7 +7,7 @@ const cpf = document.querySelector('#input-cpf');
 const endereco = document.querySelector('#input-endereco');
 const cidade = document.querySelector('#input-cidade');
 const estado = document.querySelector('#select-estado');
-const residencia = document.getElementsByName('resistecia');
+const residencia = document.getElementsByName('residencia');
 const resumoCurriculo = document.querySelector('#textarea-resumo-curriculo');
 const cargo = document.querySelector('#input-cargo');
 const descricaoCargo = document.querySelector('#input-descricao-cargo');
@@ -15,6 +15,13 @@ const dataDeInicio = document.querySelector('#input-data-inicio');
 const dadosPessoasCurriculo = document.querySelector('#capturaDosDados');
 
 const nomeMaxLength = nome.maxLength;
+const emailMaxLength = email.maxLength;
+const cpfMaxLength = cpf.maxLength;
+const enderecoMaxLength = endereco.maxLength;
+const cidadeMaxLength = cidade.maxLength;
+const resumoCurriculoMaxLength = resumoCurriculo.maxLength;
+const cargoMaxLength = cargo.maxLength;
+const descricaoCargoMaxLength = descricaoCargo.maxLength;
 
 const estados = {
 ac: 'Acre',
@@ -45,12 +52,17 @@ sp:	'São Paulo',
 se:	'Sergipe',
 to:	'Tocantins',
 };
-for (let key in estados) {
-  const e = document.createElement('option');
-  e.value = key;
-  e.innerHTML = estados[key];
-  menuEstados.appendChild(e);
+function criaEstados () {
+  menuEstados.innerHTML = '';
+  for (let key in estados) {
+    const e = document.createElement('option');
+    e.value = key;
+    e.innerHTML = estados[key];
+    menuEstados.appendChild(e);
+  }
 }
+
+criaEstados();
 
 function constroiData(event) {
   if (isNaN(event.key)) {
@@ -87,9 +99,10 @@ function validaData() {
 
 function validaDados(event) {
   event.preventDefault();
+  dadosPessoasCurriculo.innerHTML = '';
+  // Nome
   if (!nome.value || nome.value.length > nomeMaxLength) {
-    console.log('Teste!')
-    nome.maxLength = 40;
+    nome.maxLength = nomeMaxLength;
     nome.value = '';
     const mesagem = document.createElement('p');
     mesagem.classList.add('erroFormulario');
@@ -98,8 +111,168 @@ function validaDados(event) {
     nome.focus();
     return dadosPessoasCurriculo.appendChild(mesagem);
   } else {
-    console.log('Oi!')
+    const mesagem = document.createElement('p');
+    mesagem.classList.add('formularioCerto');
+    mesagem.innerHTML = `Nome: ${nome.value}`;
+    dadosPessoasCurriculo.appendChild(mesagem);
   }
+  // E-mail
+  if (!email.value || email.value.length > emailMaxLength) {
+    email.maxLength = emailMaxLength;
+    email.value = '';
+    const mesagem = document.createElement('p');
+    mesagem.classList.add('erroFormulario');
+    mesagem.innerHTML = 'O campo email está incorreto!';
+    mesagem.style.color = 'red';
+    email.focus();
+    return dadosPessoasCurriculo.appendChild(mesagem);
+  } else {
+    const mesagem = document.createElement('p');
+    mesagem.classList.add('formularioCerto');
+    mesagem.innerHTML = `E-mail: ${email.value}`;
+    dadosPessoasCurriculo.appendChild(mesagem);
+  }
+
+  // CPF
+  if (!cpf.value || cpf.value.length > cpfMaxLength) {
+    cpf.maxLength = cpfMaxLength;
+    cpf.value = '';
+    const mesagem = document.createElement('p');
+    mesagem.classList.add('erroFormulario');
+    mesagem.innerHTML = 'O campo cpf está incorreto!';
+    mesagem.style.color = 'red';
+    cpf.focus();
+    return dadosPessoasCurriculo.appendChild(mesagem);
+  } else {
+    const mesagem = document.createElement('p');
+    mesagem.classList.add('formularioCerto');
+    mesagem.innerHTML = `CPF: ${cpf.value}`;
+    dadosPessoasCurriculo.appendChild(mesagem);
+  }
+
+  // Endereço
+  if (!endereco.value || endereco.value.length > enderecoMaxLength) {
+    endereco.maxLength = enderecoMaxLength;
+    endereco.value = '';
+    const mesagem = document.createElement('p');
+    mesagem.classList.add('erroFormulario');
+    mesagem.innerHTML = 'O campo endereco está incorreto!';
+    mesagem.style.color = 'red';
+    endereco.focus();
+    return dadosPessoasCurriculo.appendChild(mesagem);
+  } else {
+    const mesagem = document.createElement('p');
+    mesagem.classList.add('formularioCerto');
+    mesagem.innerHTML = `Endereço: ${endereco.value}`;
+    dadosPessoasCurriculo.appendChild(mesagem);
+  }
+
+  // Cidade
+  if (!cidade.value || cidade.value.length > cidadeMaxLength) {
+    cidade.maxLength = cidadeMaxLength;
+    cidade.value = '';
+    const mesagem = document.createElement('p');
+    mesagem.classList.add('erroFormulario');
+    mesagem.innerHTML = 'O campo cidade está incorreto!';
+    mesagem.style.color = 'red';
+    cidade.focus();
+    return dadosPessoasCurriculo.appendChild(mesagem);
+  } else {
+    const mesagem = document.createElement('p');
+    mesagem.classList.add('formularioCerto');
+    mesagem.innerHTML = `Cidade: ${cidade.value}`;
+    dadosPessoasCurriculo.appendChild(mesagem);
+  }
+
+  // Estados
+  if (!estados[estado.value]) {
+    criaEstados();
+    const mesagem = document.createElement('p');
+    mesagem.classList.add('erroFormulario');
+    mesagem.innerHTML = 'O campo estado está incorreto!';
+    mesagem.style.color = 'red';
+    estado.focus();
+    return dadosPessoasCurriculo.appendChild(mesagem);
+  } else {
+    const mesagem = document.createElement('p');
+    mesagem.classList.add('formularioCerto');
+    mesagem.innerHTML = `Estado: ${estados[estado.value]}`;
+    dadosPessoasCurriculo.appendChild(mesagem);
+  }
+
+  // Tipo de residencia
+  if (!residencia[0].checked && !residencia[1].checked) {
+    residencia[0].checked = true;
+    const mesagem = document.createElement('p');
+    mesagem.classList.add('erroFormulario');
+    mesagem.innerHTML = 'O campo tipo de residencia está incorreto!';
+    mesagem.style.color = 'red';
+    residencia[0].focus();
+    return dadosPessoasCurriculo.appendChild(mesagem);
+  } else {
+    let estadoTrue = '';
+    for (let i = 0; i < residencia.length; i += 1) {
+      if (residencia[i].checked) {
+        estadoTrue = residencia[i];
+      }
+    }
+    const mesagem = document.createElement('p');
+    mesagem.classList.add('formularioCerto');
+    mesagem.innerHTML = `Tipo de residencia: ${estadoTrue.parentNode.innerText}`;
+    dadosPessoasCurriculo.appendChild(mesagem);
+  }
+
+  // Resumo do currículo:
+  if (!resumoCurriculo.value || resumoCurriculo.value.length > resumoCurriculoMaxLength) {
+    resumoCurriculo.maxLength = resumoCurriculoMaxLength;
+    resumoCurriculo.value = '';
+    const mesagem = document.createElement('p');
+    mesagem.classList.add('erroFormulario');
+    mesagem.innerHTML = 'O campo resumo currículo está incorreto!';
+    mesagem.style.color = 'red';
+    resumoCurriculo.focus();
+    return dadosPessoasCurriculo.appendChild(mesagem);
+  } else {
+    const mesagem = document.createElement('p');
+    mesagem.classList.add('formularioCerto');
+    mesagem.innerHTML = `Resumo Currículo: ${resumoCurriculo.value}`;
+    dadosPessoasCurriculo.appendChild(mesagem);
+  }
+
+   // Cargo:
+   if (!cargo.value || cargo.value.length > cargoMaxLength) {
+    cargo.maxLength = cargoMaxLength;
+    cargo.value = '';
+    const mesagem = document.createElement('p');
+    mesagem.classList.add('erroFormulario');
+    mesagem.innerHTML = 'O campo cargo está incorreto!';
+    mesagem.style.color = 'red';
+    cargo.focus();
+    return dadosPessoasCurriculo.appendChild(mesagem);
+  } else {
+    const mesagem = document.createElement('p');
+    mesagem.classList.add('formularioCerto');
+    mesagem.innerHTML = `Cargo: ${cargo.value}`;
+    dadosPessoasCurriculo.appendChild(mesagem);
+  }
+
+  // Descricão Cargo:
+  if (!descricaoCargo.value || descricaoCargo.value.length > descricaoCargoMaxLength) {
+    descricaoCargo.maxLength = descricaoCargoMaxLength;
+    descricaoCargo.value = '';
+    const mesagem = document.createElement('p');
+    mesagem.classList.add('erroFormulario');
+    mesagem.innerHTML = 'O campo descrição cargo está incorreto!';
+    mesagem.style.color = 'red';
+    descricaoCargo.focus();
+    return dadosPessoasCurriculo.appendChild(mesagem);
+  } else {
+    const mesagem = document.createElement('p');
+    mesagem.classList.add('formularioCerto');
+    mesagem.innerHTML = `Descricão Cargo: ${descricaoCargo.value}`;
+    dadosPessoasCurriculo.appendChild(mesagem);
+  }
+  dadosPessoasCurriculo.lastChild.focus();
 }
 
 dataDeInicio.addEventListener('keypress', constroiData);
