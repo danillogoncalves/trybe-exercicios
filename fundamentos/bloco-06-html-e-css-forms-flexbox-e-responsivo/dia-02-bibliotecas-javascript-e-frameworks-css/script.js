27f6262e458b9e8e4377e88e7f80809ab27008e1
danillo <danillogonvalves001@gmail.com>
2022-01-09 22:53:06 -0300

    Finalizei todo o exercicio.

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/script.js
@@ -12,229 +12,221 @@ const resumoCurriculo = document.querySelector('#textarea-resumo-curriculo');
 const cargo = document.querySelector('#input-cargo');
 const descricaoCargo = document.querySelector('#input-descricao-cargo');
 const dataDeInicio = document.querySelector('#input-data-inicio');
-const dadosPessoasCurriculo = document.querySelector('#capturaDosDados');
-
-const nomeMaxLength = nome.maxLength;
-const emailMaxLength = email.maxLength;
-const cpfMaxLength = cpf.maxLength;
-const enderecoMaxLength = endereco.maxLength;
-const cidadeMaxLength = cidade.maxLength;
-const resumoCurriculoMaxLength = resumoCurriculo.maxLength;
-const cargoMaxLength = cargo.maxLength;
-const descricaoCargoMaxLength = descricaoCargo.maxLength;
+const relatorioDosDados = document.querySelector('#relatorio-dos-dados');
 
 const estados = {
-ac: 'Acre',
-al:	'Alagoas',
-ap:	'Amapá',
-am:	'Amazonas',
-ba: 'Bahia',
-ce:	'Ceara',
-df: 'Distrito Federal',
-es:	'Espírito Santo',
-go:	'Goiás',
-ma:	'Maranhão',
-mt:	'Mato Grosso',
-ms:	'Mato Grosso do Sul',
-mg:	'Minas Gerais',
-pa:	'Pará',
-pb:	'Paraíba',
-pr:	'Paraná',
-pe:	'Pernambuco',
-pi:	'Piauí',
-rj:	'Rio de Janeiro',
-rn:	'Rio Grande do Norte',
-rs:	'Rio Grande do Sul',
-ro:	'Rondônia',
-rr:	'Roraima',
-sc:	'Santa Catarina',
-sp:	'São Paulo',
-se:	'Sergipe',
-to:	'Tocantins',
+  ac: 'Acre',
+  al:	'Alagoas',
+  ap:	'Amapá',
+  am:	'Amazonas',
+  ba: 'Bahia',
+  ce:	'Ceara',
+  df: 'Distrito Federal',
+  es:	'Espírito Santo',
+  go:	'Goiás',
+  ma:	'Maranhão',
+  mt:	'Mato Grosso',
+  ms:	'Mato Grosso do Sul',
+  mg:	'Minas Gerais',
+  pa:	'Pará',
+  pb:	'Paraíba',
+  pr:	'Paraná',
+  pe:	'Pernambuco',
+  pi:	'Piauí',
+  rj:	'Rio de Janeiro',
+  rn:	'Rio Grande do Norte',
+  rs:	'Rio Grande do Sul',
+  ro:	'Rondônia',
+  rr:	'Roraima',
+  sc:	'Santa Catarina',
+  sp:	'São Paulo',
+  se:	'Sergipe',
+  to:	'Tocantins',
 };
 function criaEstados () {
   menuEstados.innerHTML = '';
   for (let key in estados) {
     const e = document.createElement('option');
-    e.value = key;
+    e.value = estados[key];
     e.innerHTML = estados[key];
     menuEstados.appendChild(e);
   }
 }
 
+const validate = new window.JustValidate('#form');
+
+dataDeInicio.DatePickerX.init({
+  format: 'dd/mm/yyyy',
+});
+
 criaEstados();
 
-function constroiData(event) {
-  if (isNaN(event.key)) {
-    event.preventDefault()
-  }
-  const tamanho = dataDeInicio.value.length;
-  if (tamanho === 2 || tamanho === 5) {
-    dataDeInicio.value = dataDeInicio.value + '/';
-  }
-}
-
-function validaDia(array) {
-  if(array[0] < 1 || array[0] >31) {
-    return true;
-  }
-}
-function validaMes(array) {
-  if(array[1] < 1 || array[1] >12) {
-    return true;
-  }
-}
-function validaAno(array) {
-  if (array[2] < 0) {
-    return true;
-  }
-}
-function validaData() {
-  const valor = dataDeInicio.value.match(/\d+/g);
-  if (!valor) {
-    alert('Campo Data de Início está vázio!');
-    dataDeInicio.value = '';
-    return
-  }
-  if (validaDia(valor) || validaMes(valor) || validaAno(valor)) {
-    alert('Essa data é invalida!');
-    dataDeInicio.value = '';
-  }
-
-}
-
-function validaDados(event) {
-  event.preventDefault();
-  dadosPessoasCurriculo.innerHTML = '';
-  
-  // Nome
-  if (!nome.value || nome.value.length > nomeMaxLength) {
-    return mensagemDeErro('nome', nome, nomeMaxLength);
-  } else {
-    mensagemRelatorio("Nome", nome);
-  }
-  
-  // E-mail
-  if (!email.value || email.value.length > emailMaxLength) {
-    return mensagemDeErro('e-mail', email, emailMaxLength);
-  } else {
-    mensagemRelatorio("E-mail", email);
-  }
-
-  // CPF
-  if (!cpf.value || cpf.value.length > cpfMaxLength) {
-    return mensagemDeErro('CPF', cpf, cpfMaxLength);
-  } else {
-    mensagemRelatorio("CPF", cpf);
-  }
-
-  // Endereço
-  if (!endereco.value || endereco.value.length > enderecoMaxLength) {
-    return mensagemDeErro('endereço', endereco, enderecoMaxLength);
-  } else {
-    mensagemRelatorio("Endereço", endereco);
-  }
- 
-  // Cidade
-  if (!cidade.value || cidade.value.length > cidadeMaxLength) {
-    return mensagemDeErro('cidade', cidade, cidadeMaxLength);
-  } else {
-    mensagemRelatorio("Cidade", cidade);
-  }
-
-  // Estados
-  if (!estados[estado.value]) {
-    criaEstados();
-    const mesagem = document.createElement('p');
-    mesagem.classList.add('erroFormulario');
-    mesagem.innerHTML = 'O campo estado está incorreto!';
-    mesagem.style.color = 'red';
-    estado.focus();
-    return dadosPessoasCurriculo.appendChild(mesagem);
-  } else {
-    const mesagem = document.createElement('p');
-    mesagem.classList.add('formularioCerto');
-    mesagem.innerHTML = `Estado: ${estados[estado.value]}`;
-    dadosPessoasCurriculo.appendChild(mesagem);
-  }
-
-  // Tipo de residencia
-  if (!residencia[0].checked && !residencia[1].checked) {
-    residencia[0].checked = true;
-    const mesagem = document.createElement('p');
-    mesagem.classList.add('erroFormulario');
-    mesagem.innerHTML = 'O campo tipo de residencia está incorreto!';
-    mesagem.style.color = 'red';
-    residencia[0].focus();
-    return dadosPessoasCurriculo.appendChild(mesagem);
-  } else {
-    let estadoTrue = '';
-    for (let i = 0; i < residencia.length; i += 1) {
-      if (residencia[i].checked) {
-        estadoTrue = residencia[i];
-      }
+validate
+  .addField('#input-nome', [
+    {
+      rule: 'required',
+      errorMessage: 'Nome é obrigatório',
+    },
+    {
+      rule: 'minLength',
+      value: 3,
+      errorMessage: 'O campo deve conter no mínimo 3 caracteres',
+    },
+    {
+      rule: 'maxLength',
+      value: 40,
+      errorMessage: 'O campo deve conter no máximo 40 caracteres'
     }
-    const mesagem = document.createElement('p');
-    mesagem.classList.add('formularioCerto');
-    mesagem.innerHTML = `Tipo de residencia: ${estadoTrue.parentNode.innerText}`;
-    dadosPessoasCurriculo.appendChild(mesagem);
-  }
+  ])
+  .addField('#input-email', [
+    {
+      rule: 'required',
+      errorMessage: 'E-mail é obrigatório',
+    },
+    {
+      rule: 'email',
+      errorMessage: 'E-mail inválido!',
+    },
+    {
+      rule: 'maxLength',
+      value: 50,
+      errorMessage: 'O campo deve conter no máximo 50 caracteres'
+    }
+  ])
+  .addField('#input-cpf', [
+    {
+      rule: 'required',
+      errorMessage: 'CPF é obrigatório'
+    },
+    {
+      rule: 'number',
+      errorMessage: 'O valor deve ser um número',
+    },
+    {
+      rule: 'maxLength',
+      value: 11,
+      errorMessage: 'O campo deve conter no máximo 11 caracteres'
+    }
+  ])
+  .addField('#input-endereco', [
+    {
+      rule: 'required',
+      errorMessage: 'Endereço é obrigatório'
+    },
+    {
+      rule: 'maxLength',
+      value: 200,
+      errorMessage: 'O campo deve conter no máximo 200 caracteres'
+    }
+  ])
+  .addField('#input-cidade', [
+    {
+      rule: 'required',
+      errorMessage: 'Cidade é obrigatório'
+    },
+    {
+      rule: 'maxLength',
+      value: 28,
+      errorMessage: 'O campo deve conter no máximo 28 caracteres'
+    }
+  ])
+  .addField('#select-estado', [
+    {
+      rule: 'required',
+      errorMessage: 'Estados é obrigatório'
+    }
+  ])
+  .addRequiredGroup('#residencias',
+    'Tipos de residência é obrigatório'
+  )
+  .addField('#textarea-resumo-curriculo', [
+    {
+      rule: 'required',
+      errorMessage: 'Resumo do currículo é obrigatório'
+    },
+    {
+      rule: 'maxLength',
+      value: 1000,
+      errorMessage: 'O campo deve conter no máximo 1000 caracteres'
+    }
+  ])
+  .addField('#input-cargo', [
+    {
+      rule: 'required',
+      errorMessage: 'Cargo é obrigatório'
+    },
+    {
+      rule: 'maxLength',
+      value: 40,
+      errorMessage: 'O campo deve conter no máximo 40 caracteres'
+    }
+  ])
+  .addField('#input-descricao-cargo', [
+    {
+      rule: 'required',
+      errorMessage: 'Descrição do cargo é obrigatório'
+    },
+    {
+      rule: 'maxLength',
+      value: 500,
+      errorMessage: 'O campo deve conter no máximo 500 caracteres'
+    }
+  ])
+  .addField('#input-data-inicio', [
+    {
+      rule: 'required',
+      errorMessage: 'Data de Início é obrigatório'
+    }
+  ])
+  .onSuccess((event) => {
+    geraRelatorio();
+  });
+;
 
-  // Resumo do currículo:
-  if (!resumoCurriculo.value || resumoCurriculo.value.length > resumoCurriculoMaxLength) {
-    return mensagemDeErro('resumo currículo', resumoCurriculo, resumoCurriculoMaxLength);
-  } else {
-    mensagemRelatorio("Resumo Currículo", resumoCurriculo);
-  }
-
-   // Cargo:
-  if (!cargo.value || cargo.value.length > cargoMaxLength) {
-    return mensagemDeErro('cargo', cargo, cargoMaxLength);
-  } else {
-    mensagemRelatorio("Cargo", cargo);
-  }
-
-  // Descricão Cargo:
-  if (!descricaoCargo.value || descricaoCargo.value.length > descricaoCargoMaxLength) {
-    return mensagemDeErro('descrição cargo', descricaoCargo, descricaoCargoMaxLength);
-  } else {
-    mensagemRelatorio("Descricão Cargo", descricaoCargo);
-  }
-  //Data de Início
-  if (!dataDeInicio.value) {
-    const mesagem = document.createElement('p');
-    mesagem.classList.add('erroFormulario');
-    mesagem.innerHTML = `O campo data de início está vázio!`;
-    mesagem.style.color = 'red';
-    dataDeInicio.focus();
-    return dadosPessoasCurriculo.appendChild(mesagem)
-  }
-  const mesagem = document.createElement('p');
-  mesagem.classList.add('formularioCerto');
-  mesagem.innerHTML = `Data de Início: ${dataDeInicio.value}`;
-  dadosPessoasCurriculo.appendChild(mesagem);
-
-  window.location.href = '#capturaDosDados';
+function criaRelatorioDadosPessoais(item, valor) {
+  const div = document.createElement('div');
+  const span = document.createElement('span');
+  span.innerHTML = `${item}: ${valor}`;
+  div.appendChild(span);
+  relatorioDosDados.appendChild(div);
 }
-// Desenvolvido jundo como Roberval Filho na mentoria
-function mensagemDeErro(nome, element, maxLength) {
-  element.maxLength = maxLength;
-  element.value = '';
-  const mesagem = document.createElement('p');
-  mesagem.classList.add('erroFormulario');
-  mesagem.innerHTML = `O campo ${nome} está incorreto!`;
-  mesagem.style.color = 'red';
-  element.focus();
-  return dadosPessoasCurriculo.appendChild(mesagem);
+
+function criaRelatorioDadosProfissionais(item, valor) {
+  const div = document.createElement('div');
+  const span = document.createElement('span');
+  span.innerHTML = `${item}: ${valor}`;
+  div.appendChild(span);
+  relatorioDosDados.appendChild(div);
 }
 
-function mensagemRelatorio(nome, element) {
-  const mesagem = document.createElement('p');
-  mesagem.classList.add('formularioCerto');
-  mesagem.innerHTML = `${nome}: ${element.value}`;
-  dadosPessoasCurriculo.appendChild(mesagem);
+function geraRelatorio() {
+  relatorioDosDados.innerHTML = '';
+  relatorioDosDados.className = 'container flex-shrink-1 border m-1 pt-3';
+  const listaDadosPessoais = document.querySelector('#dados-pessoais').children;
+  const listaDadosProfissionais = document.querySelector('#dados-profissionais').children;
+  for (let i = 0; i < (listaDadosPessoais.length - 1); i += 1) {
+    const label = listaDadosPessoais[i].children[0].innerText;
+    const input = listaDadosPessoais[i].children[1].value;
+    criaRelatorioDadosPessoais(label, input);
+  }
+  const residencia = document.querySelector('#residencias').children;
+  const divResidencia = document.createElement('div');
+  const spanResidencia = document.createElement('span');
+  const labelResidencia = document.querySelector('#residencias').previousElementSibling.innerText;
+  let inputResidencia = '';
+  for (let i = 0; i < residencia.length; i += 1) {
+    if (residencia[i].children[1].checked) {
+      inputResidencia = residencia[i].children[0].innerText;
+      break;
+    }
+  }
+  spanResidencia.innerHTML = `${labelResidencia}: ${inputResidencia}`;
+  divResidencia.appendChild(spanResidencia);
+  relatorioDosDados.appendChild(divResidencia);
+  for (let i = 0; i < (listaDadosProfissionais.length); i += 1) {
+    const label = listaDadosProfissionais[i].children[0].innerText;
+    const input = listaDadosProfissionais[i].children[1].value;
+    criaRelatorioDadosProfissionais(label, input);
+  }
+  window.location.href = '#relatorio-dos-dados';
 }
-
-dataDeInicio.addEventListener('keypress', constroiData);
-dataDeInicio.addEventListener('change', validaData);
-botaoEnviar.addEventListener('click', validaDados);
