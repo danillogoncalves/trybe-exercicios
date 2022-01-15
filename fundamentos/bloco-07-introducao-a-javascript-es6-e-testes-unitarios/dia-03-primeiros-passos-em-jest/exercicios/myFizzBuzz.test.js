1484ee7248fa8bc8f380d1f15528066a82495a0c
danillo <danillogonvalves001@gmail.com>
2022-01-15 20:19:41 -0300

    Exercício 3, parte 4 feito.

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/exercicios/myFizzBuzz.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/exercicios/myFizzBuzz.test.js
@@ -1,5 +1,4 @@
 const { it, expect } = require('@jest/globals');
-const exp = require('constants');
 const myFizzBuzz = require('./myFizzBuzz');
 
 describe('3 - A função myFizzBuzz(num) recebe um número num e retorna "fizzbuzz" se o número for divisível por 3 e 5 , retorna "fizz" se for divisível apenas por 3 , retorna "buzz" se divisível apenas por 5 , retorna o próprio número caso não seja divisível por 3 ou 5 e retorna false caso num não seja um número', () => {
@@ -11,5 +10,8 @@ describe('3 - A função myFizzBuzz(num) recebe um número num e retorna "fizzbu
   });
   it('3. Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado', () => {
     expect(myFizzBuzz(5)).toBe('buzz');
-  })
+  });
+  it('4. Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(4)).toBe(4);
+  });
 });
