268d5a37ee24e895b3fef0b629597c2de965cdd9
danillo <danillogonvalves001@gmail.com>
2022-01-15 19:30:58 -0300

    Exercício 2, parte 3 feito.

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/exercicios/myRemove.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/exercicios/myRemove.test.js
@@ -8,4 +8,7 @@ describe('2 - A função myRemove(arr, item) recebe um array arr e retorna uma c
   it('2. Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
     expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
   });
+  it('3. Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
 });
