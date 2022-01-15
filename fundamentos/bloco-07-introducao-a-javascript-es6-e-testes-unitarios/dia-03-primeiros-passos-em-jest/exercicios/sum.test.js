93ce10884e383d3ba62cb20d401a40a149727f16
danillo <danillogonvalves001@gmail.com>
2022-01-15 18:22:05 -0300

    Exercício 1, parte 3 feito.

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/exercicios/sum.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/exercicios/sum.test.js
@@ -1,11 +1,14 @@
 const { it, expect } = require('@jest/globals');
 const sum = require('./sum');
 
-describe('A função sum(a, b) retorna a soma do parâmetro a com o b', () => {
-  it('Teste se o retorno de sum(4, 5) é 9', ()=> {
+describe('1 - A função sum(a, b) retorna a soma do parâmetro a com o b', () => {
+  it('1. Teste se o retorno de sum(4, 5) é 9', ()=> {
     expect(sum(4, 5)).toBe(9);
   });
-  it('Teste se o retorno de sum(0, 0) é 0', () => {
+  it('2. Teste se o retorno de sum(0, 0) é 0', () => {
     expect(sum(0, 0)).toBe(0);
-  })
+  });
+  it('3. Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)', () => {
+    expect(() => { sum() }).toThrow();
+  });
 });
