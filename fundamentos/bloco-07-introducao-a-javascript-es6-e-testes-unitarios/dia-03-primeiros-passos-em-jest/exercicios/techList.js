959888b4c43d297b18be4378500f35c063a33536
danillo <danillogonvalves001@gmail.com>
2022-01-16 18:45:41 -0300

    Exercício 5, feito.

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/exercicios/techList.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/exercicios/techList.js
@@ -1,5 +1,16 @@
-function techList() {
+function techList(array, string) {
+  // seu código aqui
+  let arraySort = array.sort();
+  let arrayPrint = [];
+  let personalTech = {};
 
+  for (let i = 0; i < arraySort.length; i += 1) {
+    personalTech.tech = arraySort[i];
+    personalTech.name = string;
+    arrayPrint.push(personalTech);
+    personalTech = {};
+  }
+  return arrayPrint.length === 0 ? 'Vazio!' : arrayPrint;
 }
 
 module.exports = techList;
