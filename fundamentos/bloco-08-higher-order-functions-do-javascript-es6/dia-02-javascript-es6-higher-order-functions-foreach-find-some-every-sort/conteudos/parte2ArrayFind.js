157d41d02d86dcfe325cfe3230f78ddb0162ff1e
danillo <danillogonvalves001@gmail.com>
2022-01-19 11:58:14 -0300

    Parte 3 da fixaçao foi feita.

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/conteudos/parte2ArrayFind.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/conteudos/parte2ArrayFind.js
@@ -19,3 +19,17 @@ const findNameWithFiveLetters = () => {
 };
 
 console.log(findNameWithFiveLetters());
+
+// Fixação 3
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+]
+
+function findMusic(id) {
+  // Adicione seu código aqui
+  return musicas.find(element => element.id === id);
+}
+
+console.log(findMusic('31031686'))
