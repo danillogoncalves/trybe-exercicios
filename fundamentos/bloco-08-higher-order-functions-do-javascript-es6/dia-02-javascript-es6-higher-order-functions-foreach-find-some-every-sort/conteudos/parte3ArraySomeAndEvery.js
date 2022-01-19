74d975afd04d6fc8c91d74cdee41d1b4add2a6c3
danillo <danillogonvalves001@gmail.com>
2022-01-19 12:19:48 -0300

    Parte 2 da fixaçao foi feita.

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/conteudos/parte3ArraySomeAndEvery.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/conteudos/parte3ArraySomeAndEvery.js
@@ -7,3 +7,20 @@ const hasName = (arr, name) => {
 }
 
 console.log(hasName(names, 'Ana'))
+
+// Fixação 2
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => {
+  //Adicione seu código aqui
+  return arr.every(element => element.age >= minimumAge);
+}
+
+console.log(verifyAges(people, 18));
