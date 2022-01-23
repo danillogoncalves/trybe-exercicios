acad7d5d8103df7f0eda84c4594aa601f5b25659
danillo <danillogonvalves001@gmail.com>
2022-01-22 21:41:57 -0300

    Fazendo a 5.

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/exercicios/exercise5.js
@@ -0,0 +1,10 @@
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+function containsA() {
+  // escreva seu código aqui
+  // names.reduce((acc, curr) => , 0)
+}
