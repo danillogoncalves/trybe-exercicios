14fea6290b37f06d56534a65e1b34495c49cff63
danillo <danillogonvalves001@gmail.com>
2022-01-24 11:20:18 -0300

    Testes feito da para do spread Operator.

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/conteudos/tests.js
@@ -0,0 +1,74 @@
+// const numbers = [1, 2, 3];
+// console.log(numbers);
+
+// numbers.push(4);
+
+// console.log(numbers); // [1, 2, 3, 4]
+
+// const carros = ['Gol', 'HB20', 'Focus'];
+// const motos = ['Biz', 'R1'];
+
+// const veiculos = ['F40', ...motos, 'Uno', ...carros].sort();
+
+// console.log(veiculos);
+
+// const conhecimentosTrybe = {
+//   softSkills: true,
+//   hardSkills: true,
+//   trabalho: true,
+// }
+
+// const pessoa = {
+//   nome: 'Nádia',
+//   idade: '28',
+//   cidade: 'BH',
+// }
+
+// const pessoaTryber = {
+//   ...pessoa,
+//   ...conhecimentosTrybe,
+//   esporte: 'Corrida',
+// }
+
+// console.log(pessoaTryber);
+
+// const numbers = [1, 2, 3];
+
+// const newArray = [...numbers, 4, 5, 6];
+
+// console.log(newArray);
+// console.log(numbers);
+
+// const spring = ['OUT', 'NOV', 'DEZ'];
+// const summer = ['JAN', 'FEV', 'MAR'];
+// const fall = ['ABR', 'MAI', 'JUN'];
+// const winter = ['JUL', 'AGO', 'SET'];
+
+// const months = [...summer, ...fall, ...winter, ...spring]
+
+// console.log(months);
+
+// const imc = (peso, altura) => (peso / (altura * altura)).toFixed(2);
+// const patientInfo = [60, 1.7];
+
+// console.log(imc(...patientInfo));
+
+// const randomNumbers = [57, 8, 5, 800, 152, 74, 630, 98, 40];
+
+// console.log(Math.max(...randomNumbers));
+// console.log(Math.min(...randomNumbers));
+
+const people = {
+  id: 101,
+  name: "Alysson",
+  age: 25,
+}
+
+const about = {
+  address: 'Av Getúlio Vargas, 1000',
+  occupation: 'Developer',
+}
+
+const customer = { ...people, ...about };
+
+console.log(customer);
