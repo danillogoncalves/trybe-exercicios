9d2a62008d2e13a10b1a4c5550ac638ac4c13a4a
danillo <danillogonvalves001@gmail.com>
2022-01-28 15:30:27 -0300

    Estudando os exercicios.

---
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/conteudos/fixar.js
@@ -0,0 +1,116 @@
+// const pushNumber = (list, number) => list.push(number);
+
+// let numbers = [];
+
+// pushNumber(numbers, 1);
+// pushNumber(numbers, 2);
+// pushNumber(numbers, 3);
+
+// console.log(numbers);
+
+// const pushNumber = (list, number) => list.push(number);
+
+// let numbers = [];
+
+// setTimeout(() => pushNumber(numbers, 1), 3000);
+// pushNumber(numbers, 2);
+// pushNumber(numbers, 3);
+
+// console.log(numbers);
+
+// const pushNumber = (list, number) => list.push(number);
+
+// let numbers = [];
+
+// setTimeout(() => pushNumber(numbers, 1), 3000);
+// pushNumber(numbers, 2);
+// pushNumber(numbers, 3);
+
+// console.log(numbers);
+// setTimeout(() => console.log(numbers), 3000);
+
+// const despesas = [
+//   {
+//     gym: 99,
+//   },
+//   {
+//     ifood: 200,
+//   },
+//   {
+//     phone: 60,
+//   },
+//   {
+//     internet: 100,
+//   },
+// ];
+
+// const renda = 1000;
+
+// const somaDasDespesas = (array) => {
+//   return array.reduce((acc, currrent) => acc + Number(Object.values(currrent)), 0);
+// }
+
+// const despesaMensal = (renda, despesas, callback) => {
+
+//   const despesaTotal = callback(despesas);
+//   const saldoFinal = renda - despesaTotal;
+
+//   console.log(`Balanço do mês:
+//     Recebido: R$${renda},00
+//     Gasto: R$${despesaTotal},00
+//     Saldo: R$${saldoFinal},00 `);
+// };
+
+// console.log(despesaMensal(renda, despesas, somaDasDespesas));
+
+// Declaramos nossa variável de despesas
+const despesas = [
+  {
+    gym: 99,
+  },
+  {
+    ifood: 200,
+  },
+  {
+    phone: 60,
+  },
+  {
+    internet: 100,
+  },
+];
+
+// Declaramos nossa renda
+const renda = 1000;
+
+const despesaMensal = (renda, despesas, callback) => {
+  // Definimos que a despesa total é igual ao valor retornado pela função callback (que é o parâmetro da nossa função)
+  // que vai receber nosso parâmetro/variável "despesas"
+  const despesaTotal = callback(despesas);
+  // Definimos nosso saldo final, que é nossa renda - nossa despesa total
+  const saldoFinal = renda - despesaTotal;
+
+  console.log(`Balanço do mês:
+    Recebido: R$${renda},00
+    Gasto: R$${despesaTotal},00
+    Saldo: R$${saldoFinal},00 `);
+};
+
+// Definimos nossa função que será passada como parâmetro
+// essa função recebe o parâmetro despesas a partir da função principal despesaMensal
+const somaDespesas = (despesas) => {
+  // Separamos cada item do nosso array de despesas
+  // e fazemos um reduce para somar os valores
+  const custoItem = despesas.map((item) => Object.values(item));
+  const despesaTotal = custoItem.reduce((acc, curr) => acc += curr[0], 0);
+  return despesaTotal;
+};
+
+// Executamos a função principal com as variáveis renda, despesas
+// e a nossa função somaDespesas
+// callback = somaDespesas
+despesaMensal(renda, despesas, somaDespesas);
+
+// Balanço do mês:
+//     Recebido: R$1000,00
+//     Gasto: R$459,00
+//     Saldo: R$541,00
