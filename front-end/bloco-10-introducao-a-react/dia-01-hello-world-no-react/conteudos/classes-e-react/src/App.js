8834aab90ac1ee74ecd5a5cb6f017817d1cbd79d
danillo <danillogonvalves001@gmail.com>
2022-02-16 16:31:58 -0300

    Feito a fixaçao do Classes e React.

---
+++ front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/conteudos/classes-e-react/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Component from './Component';
+
+function App() {
+  return <Component />;
+}
+
+export default App;
---
+++ front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/conteudos/classes-e-react/src/Component.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Component extends React.Component {
+  render() {
+    return <div>
+      <h1>Danillo Gonçalves Batista</h1>
+      <p>Olá, eu sou um estudando e futuro devenvolvedor de software Full-Stack da Trybe.</p>
+    </div>
+  }
+}
+
+export default Component;
