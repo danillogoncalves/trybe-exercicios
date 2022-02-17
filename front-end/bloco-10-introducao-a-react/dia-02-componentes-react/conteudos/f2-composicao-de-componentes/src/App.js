1fe559dabaaa71d48813de13fb2d41181d3924b2
danillo <danillogonvalves001@gmail.com>
2022-02-17 12:08:25 -0300

    Falta fazer a fixação da composição de componentes.

---
+++ front-end/bloco-10-introducao-a-react/dia-02-componentes-react/conteudos/f2-composicao-de-componentes/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Album from './components/Album';
+
+class App extends React.Component {
+  render() {
+
+    // Declaração do objeto contendo informações do album01
+    const album01 = {
+      image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Coldplay_-_Mylo_Xyloto.JPG/220px-Coldplay_-_Mylo_Xyloto.JPG',
+      title: 'Mylo Xyloto',
+      releaseDate: {
+        year: '2011',
+        month: '10',
+        day: '24',
+      },
+      others: {
+        recordCompany: 'Capitol, Parlophone',
+        formats: 'CD, digital'
+      }
+    };
+
+    // Declaração do objeto contendo informações do album02
+    const album02 = {
+      image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
+      title: 'Ghost Stories',
+      releaseDate: {
+        year: '2014',
+        month: '05',
+        day: '16',
+      },
+      others: {
+        recordCompany: 'Parlophone',
+        formats: 'CD, digital'
+      }
+    };
+
+    // Retorno do que será renderizado
+    return (
+      <div>
+        <Album album={ album01 } />
+        <Album album={ album02 } />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ front-end/bloco-10-introducao-a-react/dia-02-componentes-react/conteudos/f2-composicao-de-componentes/src/components/Album.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Album extends Component {
+  render() {
+    return (
+      <section>
+        <img src={ this.props.album.image } alt={ this.props.album.title } />
+        <h2>{ this.props.title }</h2>
+        <p>Lançamento: { this.props.album.releaseDate.year }</p>
+        <p>Gravadora: { this.props.album.others.recordCompany }</p>
+        <p>Formatos: { this.props.album.others.formats }</p>
+      </section>
+    );
+  }
+}
+
+export default Album;
