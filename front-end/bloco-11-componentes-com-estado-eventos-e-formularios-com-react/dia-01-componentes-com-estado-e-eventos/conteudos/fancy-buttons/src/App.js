81cebcdfc88f43a21d6bb5ac841e6241093121e1
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-02-21 16:30:23 -0300

    Fixação de estado e eventos.

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/conteudos/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/conteudos/fancy-buttons/src/App.js
@@ -4,19 +4,36 @@ import './App.css';
 class App extends Component {
   constructor() {
     super()
-    this.printColor = this.printColor.bind(this);
+    this.state = {
+      countOne: 0,
+      countTwo: 0,
+      countThree: 0,
+    }
+    this.handleButtonOne = this.handleButtonOne.bind(this);
+    this.handleButtonTwo = this.handleButtonTwo.bind(this);
+    this.handleButtonThree = this.handleButtonThree.bind(this);
   }
-  printColor(event) {
-    console.log(this);
-    const color = (event.target.innerText);
-    console.log(color);
+  handleButtonOne() {
+    this.setState((previousState, _props) => ({
+      countOne: previousState.countOne + 1
+    }))
+  }
+  handleButtonTwo() {
+    this.setState((previousState, _props) => ({
+      countTwo: previousState.countTwo + 1
+    }))
+  }
+  handleButtonThree() {
+    this.setState((previousState, _props) => ({
+      countThree: previousState.countThree + 1
+    }))
   }
   render() {
     return (
       <>
-        <button onClick={this.printColor}>Vermelho</button>
-        <button onClick={this.printColor}>Verde</button>
-        <button onClick={this.printColor}>Azul</button>
+        <button onClick={this.handleButtonOne}>Botão 1 - Contagem = {this.state.countOne}</button>
+        <button onClick={this.handleButtonTwo}>Botão 2 - Contagem = {this.state.countTwo}</button>
+        <button onClick={this.handleButtonThree}>Botão 3 - Contagem = {this.state.countThree}</button>
       </>
     )
   }
