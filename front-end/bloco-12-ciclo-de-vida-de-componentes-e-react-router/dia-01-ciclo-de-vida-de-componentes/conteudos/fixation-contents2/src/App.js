5e5063a1162ebbd307dbb97d09b9fc58ca7ffb1f
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-03-03 15:24:08 -0300

    Conteúdo 2 feito.

---
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-01-ciclo-de-vida-de-componentes/conteudos/fixation-contents2/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Counter from './Counter';
+
+class App extends Component {
+  render() {
+    return (
+      <Counter />
+    )
+  }
+}
+
+
+export default App;
