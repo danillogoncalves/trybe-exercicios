e6fb13c7c681ea1a3a20d14bc0b279f18d0f8c6c
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-03-24 18:34:22 -0300

    :heavy_check_mark: feat: Fixação feita.

---
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-01-introducao-ao-redux-o-estado-global-da-aplicacao/conteudos/fixar/index.js
@@ -0,0 +1,32 @@
+const Redux = require('redux');
+
+const fazerLogin = (email) => ({
+  type: "LOGIN",
+  email,
+})
+
+const ESTADO_INICIAL = {
+  login: false,
+  email: "",
+}
+
+const reducer = (state = ESTADO_INICIAL, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        ...state,
+        login: !state.login,
+        email: action.email,
+      };
+    default:
+      return state;
+  }
+}
+
+const store = Redux.createStore(reducer);
+
+console.log(store.getState());
+
+store.dispatch(fazerLogin("alguem@email.com"));
+
+console.log(store.getState());
