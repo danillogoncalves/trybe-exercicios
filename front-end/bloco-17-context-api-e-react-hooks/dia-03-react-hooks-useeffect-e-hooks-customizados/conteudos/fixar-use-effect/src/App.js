1f5fca25a9350474f58ce3f791d051c89a5d4407
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-04-14 12:10:21 -0300

    :heavy_check_mark: feat: Fixação do useEffect.

--- front-end/bloco-17-context-api-e-react-hooks/dia-03-react-hooks-useeffect-e-hooks-customizados/conteudos/fixar-use-effect/src/App.js
+++ front-end/bloco-17-context-api-e-react-hooks/dia-03-react-hooks-useeffect-e-hooks-customizados/conteudos/fixar-use-effect/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [randomNumber, setRandomNumber] = useState(0)
+  const [multiplesThreeFive, setMultiplesThreeFive] = useState(false);
+
+  useEffect(() => {
+    const thisSetInterval = setInterval(() => {
+      const random = Math.ceil(Math.random() * 100)
+      if (random % 3 === 0 || random % 5 === 0) {
+        setMultiplesThreeFive(true);
+        setTimeout(() => {
+          setMultiplesThreeFive(false)
+        }, 4000);
+      }
+      setRandomNumber(random)
+    }, 10000); 
+    return () => {
+      clearInterval(thisSetInterval);
+    }
+  }, [multiplesThreeFive]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{ randomNumber }</p>
+      {multiplesThreeFive && <p>Acerto</p>}
     </div>
   );
 }
