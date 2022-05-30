25633384e4e8cfa6f8642da84b0574e2f0d14619
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-05-30 12:26:04 -0300

    fix: Arrumando o espaçamento e alinhamento do código.

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-01-funcoes-mais-usadas-no-sql/conteudos/fixar-02.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-01-funcoes-mais-usadas-no-sql/conteudos/fixar-02.sql
@@ -1,7 +1,13 @@
 -- 1. Usando o IF na tabela sakila.film, exiba o id do filme, o título e uma coluna extra chamada 'conheço o filme?', em que deve-se avaliar se o nome do filme é 'ACE GOLDFINGER'. Caso seja, exiba "Já assisti a esse filme". Caso contrário, exiba "Não conheço o filme". Não esqueça de usar um alias para renomear a coluna da condicional.
 SELECT * FROM sakila.film;
-SELECT film_id, title,
-IF (title = 'ACE GOLDFINGER', 'Já assisti a esse filme', 'Não conheço o filme') AS 'conheço o filme?'
+SELECT
+  film_id,
+  title,
+  IF
+    (title = 'ACE GOLDFINGER',
+    'Já assisti a esse filme',
+    'Não conheço o filme')
+  AS 'conheço o filme?'
 FROM sakila.film;
 
 -- 2. Usando o CASE na tabela sakila.film, exiba o título, a classificação indicativa (rating) e uma coluna extra que vamos chamar de 'público-alvo' em que colocaremos a classificação do filme de acordo com as seguintes siglas:
@@ -14,7 +20,7 @@ SELECT
 	title,
   rating,
   CASE
-  WHEN rating = 'G' THEN 'Livre para todos'
+    WHEN rating = 'G' THEN 'Livre para todos'
     WHEN rating = 'PG' THEN 'Não recomendado para menores de 10 anos'
     WHEN rating = 'PG-13' THEN 'Não recomendado para menores de 13 anos'
     WHEN rating = 'R' THEN 'Não recomendado para menores de 17 anos'
