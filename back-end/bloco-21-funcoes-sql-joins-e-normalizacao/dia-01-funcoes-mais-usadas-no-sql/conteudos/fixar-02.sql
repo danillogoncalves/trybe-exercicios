ee129f75897afa2d749666ce8c424ee9085a21dd
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-05-30 11:51:02 -0300

    fix: Arrumando os espaçamentos.

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-01-funcoes-mais-usadas-no-sql/conteudos/fixar-02.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-01-funcoes-mais-usadas-no-sql/conteudos/fixar-02.sql
@@ -12,12 +12,12 @@ FROM sakila.film;
 -- -- Se não cair em nenhuma das classificações anteriores: "Proibido para menores de idade".
 SELECT
 	title,
-    rating,
-    CASE
-		WHEN rating = 'G' THEN 'Livre para todos'
-        WHEN rating = 'PG' THEN 'Não recomendado para menores de 10 anos'
-        WHEN rating = 'PG-13' THEN 'Não recomendado para menores de 13 anos'
-        WHEN rating = 'R' THEN 'Não recomendado para menores de 17 anos'
-        ELSE 'Proibido para menores de idade'
+  rating,
+  CASE
+  WHEN rating = 'G' THEN 'Livre para todos'
+    WHEN rating = 'PG' THEN 'Não recomendado para menores de 10 anos'
+    WHEN rating = 'PG-13' THEN 'Não recomendado para menores de 13 anos'
+    WHEN rating = 'R' THEN 'Não recomendado para menores de 17 anos'
+    ELSE 'Proibido para menores de idade'
 	END AS 'público-alvo'
 FROM sakila.film;
