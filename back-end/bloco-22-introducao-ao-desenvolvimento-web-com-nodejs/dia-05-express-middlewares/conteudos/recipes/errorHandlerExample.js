13172164b809556fe8766bd6f3a47f8a1a4636fa
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-06-10 16:23:09 -0300

    feat: Fixação de Lidando com erros feita.

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-05-express-middlewares/conteudos/recipes/errorHandlerExample.js
@@ -0,0 +1,20 @@
+/* errorHandlerExample.js */
+const express = require('express');
+const fs = require('fs/promises');
+
+const app = express();
+
+app.get('/:fileName', async (req, res, next) => {
+	try {
+		const file = await fs.readFile(`./${req.params.fileName}`);
+		res.send(file.toString('utf-8'));
+	} catch (e) {
+		next(e);
+	}
+});
+
+app.use(function (err, req, res, next) {
+  res.status(500).json({ error: `Erro: ${err.message}` });
+});
+
+app.listen(3002);
