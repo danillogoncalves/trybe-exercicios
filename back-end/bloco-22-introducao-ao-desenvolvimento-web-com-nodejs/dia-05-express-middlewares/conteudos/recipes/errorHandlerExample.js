a24e04e03bc034bbdc651554377ba317fdeb7b5b
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-06-10 18:32:15 -0300

    feat: Fixação de Pacote express-rescue feita.

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-05-express-middlewares/conteudos/recipes/errorHandlerExample.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-05-express-middlewares/conteudos/recipes/errorHandlerExample.js
@@ -1,20 +1,28 @@
 /* errorHandlerExample.js */
 const express = require('express');
+const rescue = require('express-rescue');
 const fs = require('fs/promises');
+
+const errorMiddleware = require('./errorMiddleware');
 
 const app = express();
 
-app.get('/:fileName', async (req, res, next) => {
-	try {
-		const file = await fs.readFile(`./${req.params.fileName}`);
-		res.send(file.toString('utf-8'));
-	} catch (e) {
-		next(e);
-	}
-});
+app.get('/:fileName', [
+  rescue(async (req, res) => {
+    const file = await fs.readFile(`./${req.params.fileName}`);
+    res.send(file.toString('utf-8'));
+  }),
+  (err, req, res, next) => {
+    if (err.code === 'ENOENT') {
+      const newError = new Error(err.message);
+      newError.code = 'file_not_found';
+      newError.status = 404;
+      return next(newError);
+    }
 
-app.use(function (err, req, res, next) {
-  res.status(500).json({ error: `Erro: ${err.message}` });
-});
+    return next(err);
+  },
+]);
 
+app.use(errorMiddleware);
 app.listen(3002);
