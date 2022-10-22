fb9ae5691146787bc02315eec1e7e86c2b6b8c0d
Danillo - Destra <dev5@destrasoftware.com.br>
2022-10-21 21:09:33 -0300

    feat: :white_check_mark: Exercise 4 completed.

---
+++ ciencia-da-computacao/introducao-a-python-dia-02-entrada-e-saida-de-dados/exercicios/exercise4.py
@@ -0,0 +1,49 @@
+import json
+import csv
+
+
+def retrieve_books(file):
+    return json.load(file)
+
+
+def count_books_by_categories(books):
+    categories = {}
+    for book in books:
+        for category in book["categories"]:
+            if not categories.get(category):
+                categories[category] = 0
+            categories[category] += 1
+    return categories
+
+
+def calculate_percentage_by_category(book_count_by_category, total_books):
+    return [
+        (category, num_books / total_books)
+        for category, num_books in book_count_by_category.items()
+    ]
+
+
+def write_csv_report(file, header, rows):
+    writer = csv.writer(file)
+    writer.writerow(header)
+    writer.writerows(rows)
+
+
+if __name__ == "__main__":
+    # retrieve books
+    with open("./data/books.json") as file:
+        books = retrieve_books(file)
+
+    # count by category
+    book_count_by_category = count_books_by_categories(books)
+
+    # calculate percentage
+    number_of_books = len(books)
+    books_percentage_rows = calculate_percentage_by_category(
+        book_count_by_category, number_of_books
+    )
+
+    # write csv
+    header = ["categoria", "percentagem", "Pão"]
+    with open("./data/report.csv", "w") as file:
+        write_csv_report(file, header, books_percentage_rows)
