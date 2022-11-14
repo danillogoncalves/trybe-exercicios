6ae254265ed8633a4004057607f735ce0e356bc9
danillogoncalves <danillogoncalves001@gmail.com>
2022-11-13 22:25:36 -0300

    refactor: :recycle: Melhorando a resposta.

--- secao-4-algoritmos/dia-01-complexidade-de-algoritmos/conteudos/fixar01.py
+++ secao-4-algoritmos/dia-01-complexidade-de-algoritmos/conteudos/fixar01.py
@@ -6,5 +6,18 @@ def muttply_array(numbers):
     return result
 
 
-# A Ordem de Complexidade é Ordem de Complexidade 0(1),
-# pois não importa quantidade de entradas a saída sempre será um único número.
+# Solução
+
+# Complexidade de Tempo: O(n)
+
+# Complexidade de Espaço: O(1)
+
+# Sobre a complexidade de tempo: Independentemente do tamanho da
+# entrada (numbers) o nosso laço for irá percorrer cada elemento do
+#  array de tamanho n. Por isso, sua complexidade de tempo é O(n).
+
+# Sobre a complexidade espacial, iremos operar sobre um número
+#  constante de variáveis que, por sua vez, armazenam um único
+#  elemento em memória. A variável result será responsável por armazenar
+#  o resultado numérico de nossa operação de multiplicação. Por isso,
+#  nossa complexidade espacial é O(1).
