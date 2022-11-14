41d71e78df327e62e6f61d949f1c26d6d47e850f
danillogoncalves <danillogoncalves001@gmail.com>
2022-11-13 21:58:59 -0300

    feat: :white_check_mark: Fixação de Complexidade de tempo e de espaço feita.

---
+++ secao-4-algoritmos/dia-01-complexidade-de-algoritmos/conteudos/fixar01.py
@@ -0,0 +1,10 @@
+def muttply_array(numbers):
+    result = 1
+    for number in numbers:
+        result *= number
+
+    return result
+
+
+# A Ordem de Complexidade é Ordem de Complexidade 0(1),
+# pois não importa quantidade de entradas a saída sempre será um único número.
