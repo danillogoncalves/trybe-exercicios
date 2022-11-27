4d0d69045478c975f83587adb6346e2df61a83dd
danillogoncalves <danillogoncalves001@gmail.com>
2022-11-27 19:09:33 -0300

    feat: :white_check_mark: Fixação de Estrutura de Dados: Array e Entendendo a estrutura feita.

---
+++ ciencia-da-computacao/secao-5-extrutura-de-dados-I-arrays-listas-filas-e-pilhas/dia-02-arrays/conteudos/array_example.py
@@ -0,0 +1,60 @@
+import sys
+
+"""Perceba que temos uma coleção de valores
+e operações que atuam sobre estes valores,
+de acordo com o que foi definido pelo TAD."""
+
+
+class ListaArray:
+    def __init__(self):
+        self.data = []
+
+    def __len__(self):
+        # quando pedido o tamanho do array
+        # retorne o tamanho de data
+        return len(self.data)
+
+    def __str__(self):
+        # converte para string e exibe os valores de data
+        return str(self.data)
+
+    def get(self, index):
+        # recupera o elemento no índice informado
+        return self.data[index]
+
+    def set(self, index, value):
+        # insere um elemento no índice informado
+        self.data.insert(index, value)
+
+
+# vamos inicializar e preencher uma estrutura de dados array
+array = ListaArray()
+
+# sys.getsizeof retorna o tamanho da lista em bytes
+array_memory_size = sys.getsizeof(array.data)
+print(array_memory_size)
+
+array.set(0, "Felipe")
+array.set(1, "Ana")
+
+array_memory_size = sys.getsizeof(array.data)
+print(array_memory_size)
+
+array.set(2, "Shirley")
+array.set(3, "Miguel")
+
+array_memory_size = sys.getsizeof(array.data)
+print(array_memory_size)
+
+# para acessar um elemento do array, utilizamos seu índice
+print(array.get(0))  # saída: Felipe
+print(array.get(2))  # saída: Shirley
+print("-----")
+
+# podemos iterar sobre seus elementos da seguinte maneira
+index = 0
+# enquanto há elementos no array
+while index < len(array):
+    # recupera o elemento através de um índice
+    print("Index:", index, ", Nome:", array.get(index))
+    index += 1
